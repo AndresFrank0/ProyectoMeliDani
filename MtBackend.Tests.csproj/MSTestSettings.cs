56fb13df25b2978d829097dbe7731add03b4adc5
Andrés Franquiz <52904497+AndresFrank0@users.noreply.github.com>
2025-11-17 17:05:29 -0400

    Add files via upload

---
+++ MtBackend.Tests.csproj/MSTestSettings.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class MSTestSettings
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
